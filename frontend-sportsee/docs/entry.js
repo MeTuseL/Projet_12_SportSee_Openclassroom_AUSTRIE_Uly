22bc9de68373d75c71122f07f467c6b09b7c8dc3
MeTuseL <austrie-uly@hotmail.fr>
2024-02-02 18:56:03 -0400

    docs: Documentation generated by JSDoc

---
+++ frontend-sportsee/docs/entry.js
@@ -0,0 +1,65 @@
+
+    window.reactComponents = {};
+
+    window.vueComponents = {};
+
+  
+      import React from "react";
+
+      import ReactDOM from "react-dom";
+
+
+      import ReactWrapper from '../node_modules/better-docs/lib/react-wrapper.js';
+
+      window.React = React;
+
+      window.ReactDOM = ReactDOM;
+
+      window.ReactWrapper = ReactWrapper;
+
+    
+    import './styles/reset.css';
+
+    import './styles/iframe.css';
+
+  import Component0 from '../src/components/ActivityChart/index.jsx';
+reactComponents['ActivityChart'] = Component0;
+
+import Component1 from '../src/components/AveSessionsChart/index.jsx';
+reactComponents['AveSessionsChart'] = Component1;
+
+import Component2 from '../src/components/AveSessionsChart/utils/CustomCursorAveSessions.jsx';
+reactComponents['CustomCursorAveSessions'] = Component2;
+
+import Component3 from '../src/components/PerformanceChart/utils/CustomKindLabel.jsx';
+reactComponents['CustomKindLabel'] = Component3;
+
+import Component4 from '../src/components/ScoreChart/utils/CustomLegendScore.jsx';
+reactComponents['CustomLegendScore'] = Component4;
+
+import Component5 from '../src/components/ActivityChart/utils/CustomTooltipActivity.jsx';
+reactComponents['CustomTooltipActivity'] = Component5;
+
+import Component6 from '../src/components/AveSessionsChart/utils/CustomTooltipAveSessions.jsx';
+reactComponents['CustomTooltipAveSessions'] = Component6;
+
+import Component7 from '../src/components/PerformanceChart/utils/CustomTooltipPerformance.jsx';
+reactComponents['CustomTooltipPerformance'] = Component7;
+
+import Component8 from '../src/pages/Dashboard/index.jsx';
+reactComponents['Dashboard'] = Component8;
+
+import Component9 from '../src/components/NavigationBar/index.jsx';
+reactComponents['NavigationBar'] = Component9;
+
+import Component10 from '../src/components/NutrientCard/index.jsx';
+reactComponents['NutrientCard'] = Component10;
+
+import Component11 from '../src/components/PerformanceChart/index.jsx';
+reactComponents['PerformanceChart'] = Component11;
+
+import Component12 from '../src/components/ScoreChart/index.jsx';
+reactComponents['ScoreChart'] = Component12;
+
+import Component13 from '../src/components/SpeechPresentation/index.jsx';
+reactComponents['SpeechPresentation'] = Component13;
